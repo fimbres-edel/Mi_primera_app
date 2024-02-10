e0ff3aec0659f8dd2bf4505de8940dab92a506e6
fimbres-edel <fimbres.edel@gmail.com>
2024-02-09 22:49:58 -0800

    primer commit

---
+++ src/MyComponent.js
@@ -0,0 +1,13 @@
+export function MyComponent()
+{
+  return <h1>Este es mi componente</h1>
+}
+
+export function Presentacion()
+{
+return <div>
+<h1>Nombre es Cesar Fimbres</h1>
+<h3>Mi edad es 41 años</h3>
+<p>Estudio en la UTPaquime</p>
+</div>
+}
